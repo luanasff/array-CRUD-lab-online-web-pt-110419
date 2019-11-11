636aa84158c44ff04163df7fdecfd9c5ceab2431
Luana Faria Lane <luanasfff@gmail.com>
2019-11-11 00:05:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,29 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  create_an_array = ["element1","element2","element3","element4"]
 end
 
 def add_element_to_end_of_array(array, element)
+  add_element_to_end_of_array = [array]
+  add_element_to_end_of_array << "arrays!"
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+ array =["I", "am", "really", "learning", "arrays!"]
+  add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array = [array]
+  array=remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
