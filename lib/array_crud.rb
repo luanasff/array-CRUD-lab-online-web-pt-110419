e023a860a10d843e1e36ab5997649ea625d8c2e2
Luana Faria Lane <luanasfff@gmail.com>
2019-11-11 00:11:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index = array
+  retrieve_element_from_index = [array, index_number]
   retrieve_element_from_index[1]
   
 end
