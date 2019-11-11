2ef4ceb94d7b8c5f6340e73908b71a80befa01ca
Luana Faria Lane <luanasfff@gmail.com>
2019-11-11 00:06:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
- array =["I", "am", "really", "learning", "arrays!"]
+ add_element_to_start_of_array = [array]
   add_element_to_start_of_array.unshift("wow")
 end
 
